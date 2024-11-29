7f274a1dbe3adeb2f8afe6fe2c2b4229b548b55f
Brandon Han <brandontthan2005@gmail.com>
2024-11-28 19:56:27 -0600

    Started A2C update code

--- src/policy/A2C_Policy.java
+++ src/policy/A2C_Policy.java
@@ -15,6 +15,26 @@ public class A2C_Policy extends EpsilonGreedy {
 	}
 
 	public void step(State curr, ActionSet action, State next, double[] reward, double[] prob) {
+		int player_count = base_game.player_count();
+
+		double[] curr_param = curr.to_double_array();
+		double[] next_param = next.to_double_array();
+
+		double[] curr_value = value_network.pass(curr_param);
+		double[] next_value = value_network.pass(next_param);
+
+		double[] advantage = new double[player_count];
+		double[] critic_target = new double[player_count];
+		for(int player = 0; player < player_count; player++) {
+			critic_target[player] = reward[player] + Config.Beta * next_value[player];
+			advantage[player] = critic_target[player] - curr_value[player];
+		}
+
+		double[] critic_loss_gradient = new double[player_count];
+		for(int player = 0; player < player_count; player++) {
+			critic_loss_gradient[player] = critic_target[player] - curr_value[player];
+		}
+		value_network.backpropogate(curr_param, critic_loss_gradient);
 	}
 
 	public ActionDistribution[] evaluate(State state) {
